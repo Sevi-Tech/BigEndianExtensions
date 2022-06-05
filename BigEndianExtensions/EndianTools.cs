38df03bfb429113884678a18ab7e4cdc3d1a0fce
João Severino <JoaoSeverino@users.noreply.github.com>
2022-06-05 14:16:58 +0100

    Add base project
    Include EndianTools abstract class to be used as base for all implementations

---
+++ BigEndianExtensions/EndianTools.cs
@@ -0,0 +1,22 @@
+﻿namespace BigEndianExtensions
+{
+	public abstract class EndianTools
+	{
+		public bool IsBigEndian { get; protected set; } = false;
+
+		protected byte[] GetBytes(byte[] input)
+		{
+			if (!IsBigEndian)
+			{
+				return input;
+			}
+
+			int size = input.Length;
+			byte[] buffer = new byte[size];
+			Array.Copy(input, buffer, size);
+			Array.Reverse(buffer);
+
+			return buffer;
+		}
+	}
+}
